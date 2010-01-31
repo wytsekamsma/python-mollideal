083b7daf78afaec55714f00e510ed085b8ab2890
= <=>
2010-01-31 16:34:35 +0100

    converting everything into a package...

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 		= 'mollideal',
+	version		= '0.1-alpha',
+	description 	= 'Python API voor de iDeal service van Mollie B.V.',
+	author		= 'Wytse Kamsma',
+	author_email	= 'wytse@hethogenoorden.nl',
+	url		= 'http://github.com/wytsekamsma/python-mollideal',
+	packages	= ['mollideal'],
+)	
